a306f0923afbbcc24dc6c48dd6a7c25d238eef62
hbreck <hbreckenridge@yahoo.com>
2023-07-15 11:07:08 -0400

    Create routes for content pages

--- src/App.js
+++ src/App.js
@@ -11,6 +11,9 @@ function App() {
     <>
       <Routes>
         <Route path="/" element={<Home />}/>
+        <Route path="/project" element={<Project />}/>
+        <Route path="/about" element={<About />}/>
+        <Route path="/contact" element={<Contact />}/>
       </Routes>
     </>
   );
