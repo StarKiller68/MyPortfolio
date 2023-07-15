0ffcb4f1f36ba51f0961658452c73545b656fa74
hbreck <hbreckenridge@yahoo.com>
2023-07-15 10:49:17 -0400

    Added imports for page routes

--- src/App.js
+++ src/App.js
@@ -1,5 +1,9 @@
 import React from "react";
 import "./index.css";
+import Home from "./routes/Home";
+import About from "./routes/About";
+import Project from "./routes/Project";
+import Contact from "./routes/Contact";
 
 function App() {
   return (
